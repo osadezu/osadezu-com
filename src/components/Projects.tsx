65000c46d3830edbc482361ab4c33a45ab3cb330
osadezu <osadezu@gmail.com>
2022-03-08 14:05:36 -0500

    Complete projects details

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -12,11 +12,10 @@ export interface ProjectType {
 const osdz_projects: ProjectType[] = [
   {
     name: 'PizzaParty',
-    blurb: 'A fast and easy way to for new remote teams to break the zoom ice.',
+    blurb: 'A fast and easy way for new remote teams to break the zoom ice.',
     description:
-      'A fast and easy way for new remote teams to break the zoom ice. Through a shared link, the group quickly throws together an interactive album, immediately dipping their toes into the collab spirit. During their first few days working together, members can revisit the page to remind themselves of who is who, where everyone lives, and what was the name of that fun person that —against all odds— hates Hawaiian.',
-    image_url:
-      'https://user-images.githubusercontent.com/24361930/155178316-d1f92adb-c5ec-4bd4-8200-e997a495fe9c.png',
+      'PizzaParty uses NEXT.js for the front-end and a back-end API based on Django REST Framework. Through a shared link, a remote team quickly throws together an interactive album, immediately dipping their toes into the collab spirit. During their first few days working together, members can revisit the page to remind themselves of who is who, where everyone lives, and what was the name of that fun person that —against all odds— hates Hawaiian.',
+    image_url: './images/screen-pizza-party.png',
     deployed: 'https://pizza-party.osadezu.com',
     github: 'https://github.com/osadezu/pizza-party',
   },
@@ -24,10 +23,9 @@ const osdz_projects: ProjectType[] = [
     name: 'Lazy Cupid',
     blurb: "An app to fulfill your sweetheart's V-Day expectations in seconds.",
     description:
-      "A silly but fully functional web app that allows someone who is short on time to fulfill their sweetheart's demands on Valentine's Day in a just-in-time and couldn't-have-been-easier fashion (relationship success not guaranteed). The user is given several options to customize a greeting card, including content obtained from the APIs listed below, and an option to add a personal message. When done, the user can copy an encoded URL which specifies the customizations so that the recipient can see it exactly as created without server-side data storage.",
-    image_url:
-      'https://user-images.githubusercontent.com/24361930/148898075-44cffc2f-dffb-4476-88bb-01549c21fcf1.png',
-    deployed: 'https://lazy-cupid.osadezu.com/',
+      'Lazy Cupid is an ultra-fast greeting card service using React.js. When the user is done customizing the card, a URL is encoded with all the details so the recipient can retrieve it without server-side storage.',
+    image_url: './images/screen-lazy-cupid.png',
+    deployed: 'https://lazy-cupid.osadezu.com',
     github: 'https://github.com/osadezu/lazy-cupid',
   },
   {
@@ -35,22 +33,28 @@ const osdz_projects: ProjectType[] = [
     blurb:
       'A memory game inspired by the ultimate electronic game from the 1980s.',
     description:
-      'A memory game inspired by the ultimate electronic game from the 1980s. The game is played in the same way as Simon Says, where the computer lights up a random sequence of buttons that the user has to repeat by memory. The sequence keeps getting longer until the user presses the wrong button.',
-    image_url:
-      'https://github.com/osadezu/fabulous-fred/raw/main/planning/screenshots/fabulous-fred-screenshot-01.png',
-    deployed: 'https://fabulous-fred.osadezu.com/',
+      'Fabulous Fred is written purely in Javascript, HTML and CSS. The game is played in the same way as Simon Says, where the computer lights up a random sequence of buttons that the user has to repeat by memory. The sequence keeps getting longer until the user presses the wrong button.',
+    image_url: './images/screen-fabulous-fred.png',
+    deployed: 'https://fabulous-fred.osadezu.com',
     github: 'https://github.com/osadezu/fabulous-fred',
   },
   {
-    name: 'Fabulous Fred',
-    blurb:
-      'A memory game inspired by the ultimate electronic game from the 1980s.',
+    name: 'Agiliteam Planner',
+    blurb: 'An experiment in collaboration and custom super-responsive CSS.',
     description:
-      'A memory game inspired by the ultimate electronic game from the 1980s. The game is played in the same way as Simon Says, where the computer lights up a random sequence of buttons that the user has to repeat by memory. The sequence keeps getting longer until the user presses the wrong button.',
-    image_url:
-      'https://github.com/osadezu/fabulous-fred/raw/main/planning/screenshots/fabulous-fred-screenshot-01.png',
-    deployed: 'https://fabulous-fred.osadezu.com/',
-    github: 'https://github.com/osadezu/fabulous-fred',
+      'AgiliTeam is an exercise in responsive and dynamic CSS layouts. The app uses React.js as the front-end and relies on an API written in Express.js for the backend. This demo is hosted on Netlify and Heroku.',
+    image_url: './images/screen-agiliteam.png',
+    deployed: 'https://agiliteam.netlify.app',
+    github: 'https://github.com/agiliteam-planner/agiliteam-front-end',
+  },
+  {
+    name: 'osadezu.com',
+    blurb: 'A personal portfolio page written in TypeScript using React.js',
+    description:
+      'osadezu.com is written in TypeScript using React.js and hosted on Github Pages. The styling is caringly handcrafted CSS using Google Fonts and Feather icons.',
+    image_url: './images/screen-osadezu-com.png',
+    deployed: 'https://www.osadezu.com',
+    github: 'https://github.com/osadezu/osadezu-com',
   },
 ];
 
