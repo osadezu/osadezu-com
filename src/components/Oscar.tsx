d1bb41b7a8ff6c867580e9a0ede0279907f6b893
osadezu <osadezu@gmail.com>
2022-06-08 16:07:20 -0400

    Edit personal statement

--- src/components/Oscar.tsx
+++ src/components/Oscar.tsx
@@ -5,16 +5,18 @@ function Oscar() {
         <h1>Hola! I'm Óscar</h1>
         <p>
           I am a resourceful and driven software engineer with great technical
-          intuition and a deep understanding of embedded systems.
+          intuition and a discerning focus on quality and process improvement.
         </p>
         <p>
-          With 11+ years as an architectural lighting designer, I&nbsp;developed
-          skills in project management, detail-oriented documentation, systems
-          troubleshooting, and a sense for the user experience.
+          After 11+ years as a lighting designer, I am transitioning to software
+          engineering with strong skills in detail-oriented documentation,
+          systems troubleshooting, project management, and great empathy for the
+          user experience.
         </p>
         <p>
-          I&nbsp;am a maker with a hands-on approach and a discerning focus on
-          quality and process improvement.
+          I am a relentless maker and an inquisitive learner who is always
+          looking under the hood. I am passionate about smart lighting, embedded
+          systems, and the IoT.
         </p>
       </div>
     </section>
